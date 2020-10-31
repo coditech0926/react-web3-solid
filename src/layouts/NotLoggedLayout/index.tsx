102a94f1062456f54bf84979c101fe7bcef84fba
coditech0926 <coditech.super926@gmail.com>
2020-11-01 03:46:14 +0800

    :fire: remove useless code

--- src/layouts/NotLoggedLayout/index.tsx
+++ src/layouts/NotLoggedLayout/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import { withWebId } from "@inrupt/solid-react-components";
-import styled from "styled-components";
 
 const NotLoggedInLayout = (props) => {
   const { component: Component, webId, ...rest } = props;
